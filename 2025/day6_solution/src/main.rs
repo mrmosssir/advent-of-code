f4312d9eb2b85f7addc4a01bb50ae7944fbe64f3
SuTo <mrmosssir@gmail.com>
2026-01-09 10:56:59 +0800

    (solution): 2025 - day6 part.2

--- 2025/day6_solution/src/main.rs
+++ 2025/day6_solution/src/main.rs
@@ -1,45 +1,54 @@
 use std::fs;
 
-fn str_to_arr(num_str: &str) -> Vec<u16> {
-    let str_arr: Vec<&str> = remove_space(num_str);
-    let mut num_arr: Vec<u16> = Vec::new();
-    for str_item in str_arr {
-        let num: u16 = str_item.parse().unwrap();
-        num_arr.push(num);
+fn exec_calculate (str_arr: &Vec<String>, formula: char) -> usize {
+    let mut result: usize = if formula == '*' { 1 } else { 0 };
+    for s in str_arr.iter() {
+        match formula {
+            '+' => {
+                result += s.parse::<usize>().unwrap_or(0);
+            },
+            '*' => {
+                result *= s.parse::<usize>().unwrap_or(1);
+            },
+            _ => {}
+        }
     }
-    return num_arr;
-}
-
-fn remove_space(source: &str) -> Vec<&str> {
-    source.split_whitespace().collect()
+    result
 }
 
 fn main() {
     let input = fs::read_to_string("src/input.txt").expect("無法讀取檔案");
     let lines: Vec<&str> = input.lines().collect();
-    let mut sum_group:Vec<Vec<u16>> = Vec::new();
-    let mut sum_result_group:Vec<usize> = Vec::new();
+    let sum_group:Vec<Vec<char>> = lines.iter().map(|line| line.chars().collect()).collect();
+    let formula_index: usize = 4;
 
-    sum_group.push(str_to_arr(lines[0]));
-    sum_group.push(str_to_arr(lines[1]));
-    sum_group.push(str_to_arr(lines[2]));
-    sum_group.push(str_to_arr(lines[3]));
-    let formula_group:Vec<&str> = remove_space(lines[4]);
+    let max_len: usize = sum_group
+        .iter()
+        .map(|s| s.len())
+        .max()
+        .unwrap_or(0);
+    let start_position: usize = max_len - 1;
 
-    for num in 0..sum_group[0].len() {
-        let sum: usize = match formula_group[num] {
-            "+" => {
-                (sum_group[0][num] as usize) + (sum_group[1][num] as usize) + (sum_group[2][num] as usize) + (sum_group[3][num] as usize)
-            },
-            "*" => {
-                (sum_group[0][num] as usize) * (sum_group[1][num] as usize) * (sum_group[2][num] as usize) * (sum_group[3][num] as usize)
-            },
-            _ => 0,
-        };
-        println!("第 {} 個結果: {}", num + 1, sum);
-        sum_result_group.push(sum);
+    let sum_group_len: usize = sum_group.len();
+    let mut total: usize = 0;
+    let mut total_buffer: Vec<String> = Vec::new();
+
+    for position in (0..=start_position).rev() {
+        let mut num_str: String = String::new();
+        for idx in 0..(sum_group_len - 1) {
+            if sum_group[idx][position] != ' ' {
+                num_str.push(sum_group[idx][position]);
+            }
+        }
+        
+        if num_str != "" {
+            total_buffer.push(num_str);
+        }
+        
+        if sum_group[formula_index][position] != ' ' {
+            total += exec_calculate(&total_buffer, sum_group[formula_index][position]);
+            total_buffer = Vec::new();
+        }
     }
-
-    let sum: usize = sum_result_group.iter().sum();
-    println!("結果: {}", sum);
+    println!("{total}")
 }
