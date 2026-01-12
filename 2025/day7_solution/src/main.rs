d372a273e4f899ed93062d99268b66cee600184e
SuTo <mrmosssir@gmail.com>
2026-01-12 15:28:24 +0800

    (solution): 2025 - day7 part.2

--- 2025/day7_solution/src/main.rs
+++ 2025/day7_solution/src/main.rs
@@ -10,21 +10,27 @@ fn main() {
 
     let some_start_position = lines[0].iter().position(|&c| c == 'S');
     let mut tachyon_position: Vec<usize> = Vec::new();
+    let mut tachyon_pascal: Vec<usize> = vec![0; lines[0].len()];
 
-    let mut split_times: usize = 0;
+    // let mut split_times: usize = 0;
 
     match some_start_position {
         Some(i) => tachyon_position.push(i),
         None => return
     }
 
+    tachyon_pascal[tachyon_position[0]] = 1;
+
     for line in 1..lines.len() {
         let mut buffer_tachyon_position: Vec<usize> = Vec::new();
         for position in tachyon_position.iter() {
             if lines[line][*position] == '^' {
-                split_times += 1;
+                let pascal_current = tachyon_pascal[*position];
                 let prev = position.saturating_sub(1);
                 let next = (position + 1).min(smoke_size - 1);
+                tachyon_pascal[prev] = tachyon_pascal[prev] + pascal_current;
+                tachyon_pascal[next] = tachyon_pascal[next] + pascal_current;
+                tachyon_pascal[*position] = 0;
                 if !buffer_tachyon_position.contains(&prev) {
                     buffer_tachyon_position.push(prev);
                 }
@@ -41,5 +47,11 @@ fn main() {
         }
     }
 
-    println!("{split_times}");
+    let mut total: usize = 0;
+
+    for pascal_item in tachyon_pascal {
+        total += pascal_item;
+    }
+
+    println!("{total}");
 }
